6e52508484c4877a44f7bd6b64f3adec2c88563a
Priyanshu-max417 <priyanshsingh12341234@gmail.com>
2026-07-05 16:18:48 +0530

    Chang DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/realtime-chat';
+  const uri = process.env.MONGO_URI || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/realtime-chat';
   await mongoose.connect(uri);
   console.log('MongoDB connected');
 };
